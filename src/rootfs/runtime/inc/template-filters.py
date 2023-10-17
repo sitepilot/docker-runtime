666307a87f0cd009cabac5d99203a152a8855cfb
Nick Jansen <n.jansen@sitepilot.io>
2023-10-17 14:16:35 +0200

    Remove j2cli filters file

--- src/rootfs/runtime/inc/template-filters.py
+++
@@ -1,9 +0,0 @@
-def to_bool(a):
-  ''' return a bool for the arg '''
-  if a is None or isinstance(a, bool):
-    return a
-  if isinstance(a, str):
-    a = a.lower()
-  if a in ('yes', 'on', '1', 'true', 1):
-    return True
-  return False
